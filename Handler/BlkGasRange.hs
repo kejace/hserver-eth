e5f5068c49e1bf3f7c123e78520f474abbe7ae90
Kieren James-Lubin <kjameslubin@tests-MacBook-Pro-2.local>
2015-04-07 18:09:08 -0700

    added a route

--- Handler/BlkGasRange.hs
+++ Handler/BlkGasRange.hs
@@ -2,5 +2,25 @@ module Handler.BlkGasRange where
 
 import Import
 
-getBlkGasRangeR :: Integer -> Integer -> Handler Html
-getBlkGasRangeR _r = error "Not yet implemented: getBlkGasRangeR"
+
+import Data.Aeson
+import qualified Blockchain.Data.DataDefs as DD
+import Data.ByteString.Lazy as BS
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Postgresql
+
+import qualified Database.Esqueleto as E
+       
+import Handler.PQuery
+import Data.List
+       
+import qualified Prelude as P
+       
+getBlkGasRangeR :: Integer -> Integer -> Handler Value
+getBlkGasRangeR g1 g2      =    do addHeader "Access-Control-Allow-Origin" "*"
+                                   blks <- runDB $ E.selectDistinct $
+                                        E.from $ \(a, t) -> do
+                                        E.where_ ( (a E.^. BlockDataRefGasUsed E.>=. E.val g1 ) E.&&. (a E.^. BlockDataRefGasUsed E.<=. E.val g2)  E.&&. ( a E.^. BlockDataRefBlockId E.==. t E.^. BlockId))
+                                        return t
+                                   returnJson $ nub $ (P.map entityVal blks) -- consider removing nub - it takes time n^{2}
