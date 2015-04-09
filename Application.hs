ba9e5fceb84f31a61d29954ff418e9f0e4ca75b5
Kieren James-Lubin <kjameslubin@tests-MacBook-Pro-2.local>
2015-04-09 14:45:11 -0700

    added BlkLast route

--- Application.hs
+++ Application.hs
@@ -48,6 +48,7 @@ import Handler.AccNonce
 import Handler.AccBalance
 import Handler.BlkDifficulty
 import Handler.BlkHash
+import Handler.BlkLast
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/BlkLast.hs
@@ -0,0 +1,27 @@
+module Handler.BlkLast where
+
+import Import
+
+import Data.Aeson
+import qualified Blockchain.Data.DataDefs as DD
+import Data.ByteString.Lazy as BS
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Postgresql
+
+import qualified Database.Esqueleto as E
+
+import Handler.PQuery
+import Data.List
+       
+import qualified Prelude as P
+
+getBlkLastR :: Integer -> Handler Value
+getBlkLastR n      =            do addHeader "Access-Control-Allow-Origin" "*"
+                                   blks <- runDB $ E.select $
+                                        E.from $ \(a, t) -> do
+                                        E.where_ (  a E.^. BlockDataRefBlockId E.==. t E.^. BlockId)
+                                        E.limit 100
+                                        E.orderBy [E.desc (a E.^. BlockDataRefNumber)]
+                                        return t
+                                   returnJson $ nub $ (P.map entityVal blks) -- consider removing nub - it takes time n^{2}
