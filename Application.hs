e69404bd5fc31f58a6e09aed61b9fd388db80b86
Kieren James-Lubin <kjameslubin@tests-MacBook-Pro-2.local>
2015-05-05 10:12:44 -0700

    added route

--- Application.hs
+++ Application.hs
@@ -53,6 +53,7 @@ import Handler.BlkHash
 import Handler.BlkLast
 import Handler.Demo
 import Handler.BlkCoinbase
+import Handler.BlkTxAddress
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/BlkTxAddress.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE DeriveDataTypeable
+           , EmptyDataDecls
+           , FlexibleContexts
+           , FlexibleInstances
+           , FunctionalDependencies
+           , MultiParamTypeClasses
+           , TypeFamilies
+           , UndecidableInstances
+           , GADTs
+ #-}
+
+module Handler.BlkTxAddress where
+
+import Import
+
+import Handler.Common 
+import Blockchain.Data.DataDefs
+import Blockchain.Data.Address
+
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BS
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Postgresql
+import Numeric
+import Blockchain.Data.RLP
+import Blockchain.Database.MerklePatricia
+import Blockchain.ExtWord
+import Blockchain.Util
+
+import Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Lazy as BS
+
+import qualified Database.Esqueleto as E
+       
+import Data.List
+
+import Control.Monad
+
+import qualified Prelude as P
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+import System.Locale
+import Data.Time
+import Data.Time.Format
+
+
+import Blockchain.Data.Address
+import Blockchain.ExtWord
+import Numeric
+
+import Yesod.Core.Handler
+
+import Debug.Trace
+import Handler.JsonJuggler
+
+getBlkTxAddressR :: Text -> Handler Value
+getBlkTxAddressR address = do
+  	           getParameters <- reqGetParams <$> getRequest
+                   liftIO $ traceIO $ show getParameters
+                   addHeader "Access-Control-Allow-Origin" "*"
+                   blks <- runDB $ E.select $
+                                        E.from $ \(blk `E.InnerJoin` bdRef `E.FullOuterJoin` rawTX) -> do
+
+                                        E.on ( bdRef E.^. BlockDataRefBlockId E.==. rawTX E.^. RawTransactionBlockId ) 
+                                        E.on ( bdRef E.^. BlockDataRefBlockId E.==. blk E.^. BlockId )                                        
+                                        E.where_ (( bdRef E.^. BlockDataRefCoinbase E.==. E.val (Address wd160)) E.||.
+                                                  ( ( rawTX E.^. RawTransactionFromAddress E.==. E.val (Address wd160))
+                                                      E.||. ( rawTX  E.^. RawTransactionToAddress E.==. E.val (Just (Address wd160) ))))
+
+                                        E.limit $ (fetchLimit)
+
+                                        E.orderBy [E.desc (bdRef E.^. BlockDataRefNumber)]
+
+                                        return blk
+                   returnJson $ nub $ P.map bToBPrime (P.map entityVal (blks :: [Entity Block])) 
+
+        where
+          ((wd160, _):_) = readHex $ T.unpack $ address ::  [(Word160,String)]
