4972c317763f49a7fa6400dd1248756c7384917c
Kieren James-Lubin <kjameslubin@tests-MacBook-Pro-2.local>
2015-05-05 10:53:01 -0700

    looks like I fixed blockId

--- Handler/BlkId.hs
+++ Handler/BlkId.hs
@@ -23,16 +23,12 @@ import qualified Prelude as P
 
 import Handler.JsonJuggler
 
-myQ :: (E.Esqueleto m expr backend) => (expr (Entity BlockDataRef), expr (Entity Block)) -> Integer -> m (expr (Entity BlockDataRef))
-myQ (a, t) n = do
-               E.where_  ( (a E.^. BlockDataRefNumber E.==. E.val n ) E.&&. ( a E.^. BlockDataRefBlockId E.==. t E.^. BlockId))
-               return a
 
 getBlkIdR :: Integer -> Handler Value
 getBlkIdR n = do addHeader "Access-Control-Allow-Origin" "*"
 
                  blks <- runDB $ E.select $ --Distinct
                          E.from $ \(a, t) -> do
-                           E.where_ ( (a E.^. BlockDataRefNumber E.==. E.val n ) E.&&. ( a E.^. BlockDataRefBlockId E.==. t E.^. BlockId)) ---- :: Handler [Entity AddressStateRef]
+                           E.where_ ( (a E.^. BlockDataRefBlockId E.==. E.val (toSqlKey (fromIntegral $ n)) ) E.&&. ( a E.^. BlockDataRefBlockId E.==. t E.^. BlockId)) ---- :: Handler [Entity AddressStateRef]
                            return a
                  returnJson $ nub $ P.map bdrToBdrPrime (P.map entityVal (blks :: [Entity BlockDataRef])) -- consider removing nub - it takes time n^{2}
